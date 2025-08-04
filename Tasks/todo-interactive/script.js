76d03486c88803cb4223d25f79f3e55f395e7e83
amazingawwal <sakeebmawwal@gmail.com>
2025-08-04 13:00:02 +0100

    core: completed DOM module

--- Tasks/todo-interactive/script.js
+++ Tasks/todo-interactive/script.js
@@ -4,19 +4,12 @@ const completedTask = document.querySelectorAll('#todo-list > li');
 completedTask.forEach(item=>(
     item.addEventListener('click', ()=>{
         // console.log(item)
-        item.style.backgroundColor = 'teal'
+        item.classList.toggle('completed')
     })
-
-    // console.log(item)
 ));
 
-// const h = document.getElementsByTagName('li')
-
-// const images = h.querySelectorAll('img')
-
-// console.log(images)
-
-// const ul = document.getElementById("todo-list");
-// const images = ul.getElementsByTagName('img');
-
-// console.log(images.length);
+// completedTask.forEach(task=>{
+//     task.onclick =  ()=>{
+//         task.classList.toggle('completed')
+//     }
+// })
